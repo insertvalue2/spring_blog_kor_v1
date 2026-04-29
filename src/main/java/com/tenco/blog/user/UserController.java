ed6deaa29daebfdb9bc9f68e0b770ec6b9668a29
김근호 <insertvalue@naver.com>
2026-04-29 16:22:37 +0900

    로그인 로그아웃 처리

--- src/main/java/com/tenco/blog/user/UserController.java
+++ src/main/java/com/tenco/blog/user/UserController.java
@@ -1,14 +1,61 @@
 package com.tenco.blog.user;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller // IoC
+@RequiredArgsConstructor // DI 처리
 public class UserController {
+    private final HttpSession httpSession;
+    private final UserRepository userRepository;
+
+    // 로그인 화면 요청
+    // 주소 설계 - http://localhost:8080/login-form
+    @GetMapping("/login-form")
+    public String loginFormPage() {
+        return "user/login-form";
+    }
+
+    // 로그인 기능 요청
+    @PostMapping("/login")
+    public String loginProc(UserRequest.LoginDTO loginDTO) {
+        // 1. 유효성 검사
+        loginDTO.validate();
+
+        User sessionUser = userRepository
+                .findByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword());
+
+        if(sessionUser == null) {
+            // 로그인 실패 (username, password) 불 일치
+            throw new IllegalArgumentException("사용자명 또는 비밀번호가 잘못 되었습니다");
+        }
+
+        // 여기에 코드가 도달 한다면 우리 DB에 정상 사용자임을 논리적으로 확인 됨.
+        httpSession.setAttribute("sessionUser", sessionUser);
+
+        System.out.println("로그인 성공");
+        System.out.println("로그인 사용자 : " + sessionUser.getUsername());
+        System.out.println("로그인 이메일 : " + sessionUser.getEmail());
+
+        return "redirect:/";
+    }
+
+
+    // 로그아웃 기능 요청
+
+    @GetMapping("/logout")
+    public String logout() {
+        // 세션 메모리에 내 정보를 없애 버림
+        // 로그 아웃
+        httpSession.invalidate();
+
+        return "redirect:/";
+    }
 
     // 회원 가입 화면 요청
     // 주소 설계 - http://localhost:8080/join-form
@@ -28,8 +75,19 @@ public class UserController {
         log.info("username " + joinDTO.getUsername());
         log.info("password " + joinDTO.getPassword());
         log.info("email " + joinDTO.getEmail());
+        // 1. 유효성 검사 하기
+        joinDTO.validate(); // 유효성 검사 ---> 오류 --> 예외 처리 넘어감
+        // 회원가입 요청 전 ==> 중복 username 검사
+        User userCheckName = userRepository.findByUsername(joinDTO.getUsername());
 
-        return "";
+        if(userCheckName != null) {
+            throw new IllegalArgumentException("이미 사용중인 username 입니다 : "
+                    + userCheckName.getUsername());
+        }
+        userRepository.save(joinDTO.toEntity());
+        // TODO
+        // 로그인 화면으로 리다이렉트 처리 예정
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/tenco/blog/user/UserRepository.java
+++ src/main/java/com/tenco/blog/user/UserRepository.java
@@ -1,10 +1,8 @@
 package com.tenco.blog.user;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 // SRP - 단일 책임의 원칙
@@ -33,10 +31,14 @@ public class UserRepository {
 //        Query query = em.createQuery(jpqlStr, User.class);
 //        query.setParameter("username", username);
 //        User userEntity = (User) query.getSingleResult();
+        try {
+            return em.createQuery(jpqlStr, User.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
 
-        return em.createQuery(jpqlStr, User.class)
-                .setParameter("username", username)
-                .getSingleResult();
     }
 
 
@@ -45,12 +47,13 @@ public class UserRepository {
         String jpqlStr = """
                 SELECT u FROM User u WHERE u.username = :username AND u.password = :password   
                 """;
-        User userEntity =  em.createQuery(jpqlStr, User.class)
-                .setParameter("username", username)
-                .setParameter("password", password)
-                .getSingleResult();
-
-        return userEntity;
+        try {
+            return em.createQuery(jpqlStr, User.class)
+                    .setParameter("username", username)
+                    .setParameter("password", password)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
     }
-
 }
